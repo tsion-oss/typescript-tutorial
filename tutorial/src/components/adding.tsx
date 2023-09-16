b460fc4c27d72f20bf5b384d233d2e93488848fc
Tsion <tsionwendmu12@gmail.com>
2023-09-16 16:28:18 -0400

    s

---
+++ tutorial/src/components/adding.tsx
@@ -0,0 +1,79 @@
+import { useState, ChangeEvent, FormEvent } from "react"
+
+
+interface Meal {
+  date: string;
+  breakfast: string;
+  lunch: string;
+  dinner: string;
+}
+
+export default function Main() {
+
+const initialMeal = {
+  date: '',
+  breakfast: '',
+  lunch: '',
+  dinner: '',
+
+}
+
+const [meal, setMeal] = useState<Meal>(initialMeal)
+
+console.log(meal)
+
+const handleSubmit = (e: FormEvent) => {
+ e.preventDefault()
+
+ console.log("Meal Submitted:", meal);
+}
+const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const { name, value } = e.target
+  console.log(value)
+
+  setMeal({ ...meal, [name]: value})
+}
+
+ return(
+   <div>
+     <header>
+
+     </header>
+     
+     <div>
+       <h1>Add new meal plan</h1>
+
+      <form onSubmit={handleSubmit}
+            >
+            <label>Breakfast</label>
+            <input type="text"
+                   onChange={handleChange}
+                   name='breakfast'
+                   value={meal.breakfast}
+                   />
+
+            <label>Lunch</label>
+            <input onChange={handleChange}
+                   name='lunch'
+                   value={meal.lunch}
+                   type="text"/>
+
+            <label>Dinner</label>
+            <input onChange={handleChange}
+                   name='dinner'
+                   value={meal.dinner}
+                   type="text"/>
+            <input type="date"
+                   value={meal.date}
+                   onChange={handleChange}
+                   name="date"/>
+
+            <input type="submit" value='submit'/>
+
+       </form>
+
+
+     </div>
+   </div>
+ )
+}
---
+++ tutorial/src/components/week.tsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+// import { View } from 'react-native'
+// import CalendarStrip from 'react-native-slideable-calendar-strip';
+
+// const week = () => {
+//     return(
+//         <View>
+//         <CalendarStrip
+//           // Pass any required props here
+//         />
+//       </View>
+//     )
+// }
+
+// export default week
